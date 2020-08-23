dd29eedee8f2d08ad444b296745c6209246d1ca4
Nixon Rios Sanchez <nixonrs@hotmail.com>
2020-08-23 17:07:51 -0500

    Taller1 ejercicio 4 area de un circulo

---
+++ AreaDeunCirculo/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace AreaDeunCirculo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_radioc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_areac = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(114, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(556, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CALCULAR EL AREA DE UN CIRCULO";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AreaDeunCirculo.Properties.Resources.circunferencia_circulo;
+            this.pictureBox1.Location = new System.Drawing.Point(478, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(301, 218);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calcular.Location = new System.Drawing.Point(478, 275);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(134, 53);
+            this.btn_calcular.TabIndex = 2;
+            this.btn_calcular.Text = "CALCULAR";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.Location = new System.Drawing.Point(645, 275);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(134, 53);
+            this.btn_limpiar.TabIndex = 3;
+            this.btn_limpiar.Text = "LIMPIAR";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(705, 406);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(83, 32);
+            this.btn_salir.TabIndex = 4;
+            this.btn_salir.Text = "SALIR";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-1, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(341, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ingrese el radio de la circunferencia";
+            // 
+            // txt_radioc
+            // 
+            this.txt_radioc.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_radioc.Location = new System.Drawing.Point(342, 78);
+            this.txt_radioc.Name = "txt_radioc";
+            this.txt_radioc.Size = new System.Drawing.Size(130, 39);
+            this.txt_radioc.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(58, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "en centimetros (cm):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(291, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "El area de la circunferencia es:";
+            this.label4.Visible = false;
+            // 
+            // lbl_areac
+            // 
+            this.lbl_areac.AutoSize = true;
+            this.lbl_areac.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_areac.Location = new System.Drawing.Point(104, 343);
+            this.lbl_areac.Name = "lbl_areac";
+            this.lbl_areac.Size = new System.Drawing.Size(73, 28);
+            this.lbl_areac.TabIndex = 9;
+            this.lbl_areac.Text = "label5";
+            this.lbl_areac.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_areac);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_radioc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_radioc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_areac;
+    }
+}
+
---
+++ AreaDeunCirculo/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AreaDeunCirculo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            txt_radioc.Text = String.Empty;
+            label4.Visible = false;
+            lbl_areac.Visible = false;
+            txt_radioc.Focus();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            if (txt_radioc.Text == "")
+            {
+                MessageBox.Show("Necesita ingresar el radio de la circunferencia", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                double radio, area;
+                const double PI = 3.1415926535897931;
+                radio = double.Parse(txt_radioc.Text);
+
+                area = PI * Math.Pow(radio, 2);
+
+                lbl_areac.Text = Convert.ToString(String.Format("{0:n3}", area)) + " cm";
+
+                label4.Visible = true;
+                lbl_areac.Visible = true;
+            }
+
+        }
+    }
+}
